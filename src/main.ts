628f1aa30f65aadba5c93be8427c81855766d90f
codetivated <melisa@heying.us>
2025-09-30 11:52:41 -0500

    add a note for an alternative way to inject a service

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 
+// this is no longer needed since we are providing the service in 'root'
+// bootstrapApplication(AppComponent, {
+//   providers: [TasksService],
+// }).catch((err) => console.error(err));
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
