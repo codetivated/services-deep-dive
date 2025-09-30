34c162d4308b599037f7c204532c479d92c55460
codetivated <melisa@heying.us>
2025-09-30 12:05:10 -0500

    add notes/example of how to inject TasksService at the tasks component level (this is an element injector)

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,11 +2,20 @@ import { Component } from '@angular/core';
 
 import { NewTaskComponent } from './new-task/new-task.component';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
+// import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   imports: [NewTaskComponent, TasksListComponent],
+  // providers: [TasksService], <-- see comment below
 })
 export class TasksComponent {}
+
+// injecting the service here at the tasks component level (this is an element injector)
+// so that the same instance of the service is shared between NewTaskComponent and TasksListComponent
+// If we did not provide it at this level and only on the component level, then each component would get its own instance of the service
+// because they are both standalone components and each have their own injector tree
+// By providing it here, we ensure that both components share the same instance of the service
+// which is what we want in this case since they need to share state (the list of tasks)
